f1e1f893ed2952175ae14ca27d6efa12a2407fb7
Guilherme Eiras <guialbertoeiras@hotmail.com>
2018-06-25 23:40:49 -0300

    Added logic on server to render boletim

--- server.rb
+++ server.rb
@@ -1,7 +1,21 @@
 require 'sinatra'
+require 'rabl'
+require './app/clients/fiap_agent'
+require './app/serializers/boletim_serializer'
 
-configure { set :server, :puma }
+configure do
+  set :server, :puma
+  set :views, "#{settings.root}/app/views"
+end
+
+Rabl.register!
+
+error InvalidFIAPCredentials do
+  status 401
+  "Nenhum usuário encontrado"
+end
 
 post '/notas' do
-  "Hello World!"
+  @boletim = BoletimSerializer.(FIAPAgent.new(params["rm"], params["senha"]).boletim)
+  rabl :notas, format: "json"
 end
